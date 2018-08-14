c1c199f38f55a61a5bf61166557ddc8fb811a554
hjumeau <harry.jumeau@gmail.com>
2018-08-14 14:29:57 +0200

    Use timeout

--- packages/react-app/src/App.test.js
+++ packages/react-app/src/App.test.js
@@ -2,9 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-it('renders without crashing', (done) => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-  done();
-}).timeout(200000);
+describe("long asynchronous specs", function() {
+  let originalTimeout;
+  beforeEach(function() {
+    originalTimeout = jasmine.DEFAULT_TIMEOUT_INTERVAL;
+    jasmine.DEFAULT_TIMEOUT_INTERVAL = 300000;
+  });
+
+  it('renders without crashing', (done) => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
+    ReactDOM.unmountComponentAtNode(div);
+    setTimeout(() => done(), 200000);
+  });
+
+  afterEach(function() {
+    jasmine.DEFAULT_TIMEOUT_INTERVAL = originalTimeout;
+  });
+});
